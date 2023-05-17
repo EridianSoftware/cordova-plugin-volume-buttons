ad12f02ccf092516976ecacef166d11092036dc5
Letlhogonolo Moeng <mtlhogi@gmail.com>
2023-05-17 13:52:52 +0200

    long press signals

--- src/android/VolumeButtonsListener.java
+++ src/android/VolumeButtonsListener.java
@@ -31,170 +31,172 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 
-
 public class VolumeButtonsListener extends CordovaPlugin implements OnKeyListener {
 
-	private static final String VolumeButtonsListener_LOG= "VolumeButtonsListener";	
-	private CallbackContext volumeCallbackContext;
+  private static final String VolumeButtonsListener_LOG = "VolumeButtonsListener";
+  private CallbackContext volumeCallbackContext;
 
+  /**
+   * Constructor of the VolumeButtonsListener class
+   *
+   * @date 27/02/2014
+   * @version 0.0.1
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  public VolumeButtonsListener() {
+    volumeCallbackContext = null;
+  }
 
+  /**
+   * Method which executes the Javascript request
+   *
+   * @param action:          String object with the action to execute
+   * @param args:            JSONArray object with the arguments of the request
+   * @param callbackContext: CallbackContext object for call back into Javascript
+   *
+   * @return "boolean" which indicates if the action is valid (true) or not
+   *         (false)
+   *
+   * @date 10/12/2016
+   * @version 0.0.3
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-	/**
-	* 	Constructor of the VolumeButtonsListener class
-	* 
-	* 	@date		27/02/2014
-	* 	@version	0.0.1
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	public VolumeButtonsListener(){
-		volumeCallbackContext= null;
-	}
+    // Check the action
+    if (action.equals("start")) {
 
+      // Check if the plugin is listening the volume button events
+      if (this.volumeCallbackContext != null) {
 
-	/**
-	* 	Method which executes the Javascript request
-	*
-	*	@param		action: String object with the action to execute
-	*	@param		args: JSONArray object with the arguments of the request
-	*	@param		callbackContext: CallbackContext object for call back into Javascript
-	*
-	*	@return		"boolean" which indicates if the action is valid (true) or not (false)
-	* 
-	* 	@date		10/12/2016
-	* 	@version	0.0.3
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        callbackContext.error("Volume buttons listener already running");
+        return true;
+      }
 
-		// Check the action
-		if( action.equals("start") ){
+      // Get the reference to the callbacks and start the listening process
+      this.volumeCallbackContext = callbackContext;
+      this.webView.getView().setOnKeyListener(this);
 
-			// Check if the plugin is listening the volume button events
-			if( this.volumeCallbackContext != null ){
+      // Don't return any result now
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+      pluginResult.setKeepCallback(true);
+      this.volumeCallbackContext.sendPluginResult(pluginResult);
+      return true;
+    } else if (action.equals("stop")) {
 
-				callbackContext.error("Volume buttons listener already running");
-				return true;	
-			}
+      // Erase the callbacks reference and stop the listening process
+      sendSignal(new JSONObject(), false); // release status callback in Javascript side
+      this.volumeCallbackContext = null;
+      this.webView.getView().setOnKeyListener(null);
+      callbackContext.success();
+      return true;
+    }
 
-			// Get the reference to the callbacks and start the listening process
-			this.volumeCallbackContext= callbackContext;
-			this.webView.getView().setOnKeyListener(this);
+    return false;
+  }
 
-			// Don't return any result now
-			PluginResult pluginResult= new PluginResult(PluginResult.Status.NO_RESULT);
-			pluginResult.setKeepCallback(true);
-			this.volumeCallbackContext.sendPluginResult(pluginResult);
-			return true;
-		}
-		else if( action.equals("stop") ){
+  /**
+   * Overwritten method for Android application lifecycle. It stops the key events
+   * listening process
+   *
+   * @date 10/12/2016
+   * @version 0.0.3
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  public void onDestroy() {
 
-			// Erase the callbacks reference and stop the listening process
-			sendSignal(new JSONObject(), false); // release status callback in Javascript side
-			this.volumeCallbackContext= null;
-			this.webView.getView().setOnKeyListener(null);
-			callbackContext.success();
-			return true;			
-		}
+    // Stop the listening process
+    this.webView.getView().setOnKeyListener(null);
+  }
 
-		return false;
-	}
+  /**
+   * Overwritten method for Android application lifecycle. It stops the key events
+   * listening process
+   *
+   * @date 10/12/2016
+   * @version 0.0.3
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  public void onReset() {
 
-	
-	/**
-	* 	Overwritten method for Android application lifecycle. It stops the key events listening process
-	* 
-	* 	@date		10/12/2016
-	* 	@version	0.0.3
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	public void onDestroy(){
+    // Stop the listening process
+    this.webView.getView().setOnKeyListener(null);
+  }
 
-		// Stop the listening process
-		this.webView.getView().setOnKeyListener(null);
-	}
+  /**
+   * Overwritten method to receive the Android key events
+   *
+   * @param view:     View object who emit the signal (CordovaWebView)
+   * @param keyCode:  int with the identifier of the pressed key
+   * @param keyEvent: KeyEvent object with the information of the event
+   *
+   * @return "boolean" which indicates if the listener has consumed the event
+   *         (true) or not (false) [Always false to allow that the event
+   *         spreading]
+   *
+   * @date 10/12/2016
+   * @version 0.0.3
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
 
+    // Check if the event is equal to KEY_DOWN
+    if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+      // Check what button has been pressed
+      if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && keyEvent.isLongPress()) {
 
-	/**
-	* 	Overwritten method for Android application lifecycle. It stops the key events listening process
-	* 
-	* 	@date		10/12/2016
-	* 	@version	0.0.3
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	public void onReset(){
-		
-		// Stop the listening process
-		this.webView.getView().setOnKeyListener(null);
-	}
+        // Create a new JSONObject with the information and send it
+        JSONObject info = new JSONObject();
+        try {
+          info.put("signal", new String("volume-up press"));
+          sendSignal(info, true);
+          return true;
+        } catch (JSONException ex) {
+          Log.e(VolumeButtonsListener_LOG, ex.getMessage());
+        }
+      } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && keyEvent.isLongPress()) {
 
+        // Create a new JSONObject with the information and send it
+        JSONObject info = new JSONObject();
+        try {
+          info.put("signal", new String("volume-down press"));
+          sendSignal(info, true);
+          return true;
+        } catch (JSONException ex) {
+          Log.e(VolumeButtonsListener_LOG, ex.getMessage());
+        }
+      }
+    } else if (keyEvent.getAction() == KeyEvent.ACTION_UP
+        && (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP)) {
+      // Create a new JSONObject with the information and send it
+      JSONObject info = new JSONObject();
+      try {
+        info.put("signal", new String("volume release"));
+        sendSignal(info, true);
+        return true;
+      } catch (JSONException ex) {
+        Log.e(VolumeButtonsListener_LOG, ex.getMessage());
+      }
+    }
 
-	/**
-	* 	Overwritten method to receive the Android key events
-	*
-	*	@param		view: View object who emit the signal (CordovaWebView)
-	*	@param		keyCode: int with the identifier of the pressed key
-	*	@param		keyEvent: KeyEvent object with the information of the event
-	*
-	*	@return		"boolean" which indicates if the listener has consumed the event (true) or not (false) [Always false to allow that the event spreading]
-	* 
-	* 	@date		10/12/2016
-	* 	@version	0.0.3
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+    return false;
+  }
 
-		// Check if the event is equal to KEY_DOWN
-		if( keyEvent.getAction() == KeyEvent.ACTION_DOWN )
-		{
-			// Check what button has been pressed
-			if( keyCode == KeyEvent.KEYCODE_VOLUME_UP ){
-
-				// Create a new JSONObject with the information and send it
-				JSONObject info= new JSONObject();
-				try{
-					info.put("signal", new String("volume-up"));
-					sendSignal(info, true);
-					return true;
-				}
-				catch(JSONException ex){
-					Log.e(VolumeButtonsListener_LOG, ex.getMessage());
-				}
-			}
-			else if( keyCode == KeyEvent.KEYCODE_VOLUME_DOWN  ){
-
-				// Create a new JSONObject with the information and send it
-				JSONObject info= new JSONObject();
-				try{
-					info.put("signal", new String("volume-down"));
-					sendSignal(info, true);
-					return true;
-				}
-				catch(JSONException ex){
-					Log.e(VolumeButtonsListener_LOG, ex.getMessage());
-				}
-			}
-		}
-
-		return false;
-	}
-
-
-	/**
-	* 	Method which sends back a new PluginResult to Javascript
-	*
-	*	@param		info: JSONObject object with the information to send back
-	*	@param		keepCallback: boolean which indicates if there will be more results
-	* 
-	* 	@date		27/02/2014
-	* 	@version	0.0.1
-	* 	@author	ManuelDeveloper(manueldeveloper@gmail.com) 
-	*/
-	private void sendSignal(JSONObject info, boolean keepCallback)
-	{
-		if( this.volumeCallbackContext != null ){
-			PluginResult result= new PluginResult(PluginResult.Status.OK, info);
-			result.setKeepCallback(keepCallback);
-			this.volumeCallbackContext.sendPluginResult(result);
-		}
-	}
+  /**
+   * Method which sends back a new PluginResult to Javascript
+   *
+   * @param info:         JSONObject object with the information to send back
+   * @param keepCallback: boolean which indicates if there will be more results
+   *
+   * @date 27/02/2014
+   * @version 0.0.1
+   * @author ManuelDeveloper(manueldeveloper@gmail.com)
+   */
+  private void sendSignal(JSONObject info, boolean keepCallback) {
+    if (this.volumeCallbackContext != null) {
+      PluginResult result = new PluginResult(PluginResult.Status.OK, info);
+      result.setKeepCallback(keepCallback);
+      this.volumeCallbackContext.sendPluginResult(result);
+    }
+  }
 }
